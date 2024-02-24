618da10d2f1fd41835b91ad2a083f022608297f7
François-Xavier Guillois <internet@fxguillois.email>
2024-02-24 22:18:21 +0100

    reboot

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const ENGRENAGES = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+ENGRENAGES.abilities = {
+  str: 'ENGRENAGES.Ability.Str.long',
+  dex: 'ENGRENAGES.Ability.Dex.long',
+  con: 'ENGRENAGES.Ability.Con.long',
+  int: 'ENGRENAGES.Ability.Int.long',
+  wis: 'ENGRENAGES.Ability.Wis.long',
+  cha: 'ENGRENAGES.Ability.Cha.long',
+};
+
+ENGRENAGES.abilityAbbreviations = {
+  str: 'ENGRENAGES.Ability.Str.abbr',
+  dex: 'ENGRENAGES.Ability.Dex.abbr',
+  con: 'ENGRENAGES.Ability.Con.abbr',
+  int: 'ENGRENAGES.Ability.Int.abbr',
+  wis: 'ENGRENAGES.Ability.Wis.abbr',
+  cha: 'ENGRENAGES.Ability.Cha.abbr',
+};
---
+++ module/helpers/settings.mjs
@@ -0,0 +1,27 @@
+export const registerSettings = function () {
+    game.settings.register("engrenages", "game-mode", {
+        name: game.i18n.localize("SETTINGS.world.game_mode.name"),
+        hint: game.i18n.localize("SETTINGS.world.game_mode.hint"),
+        scope: "system",
+        config: true,
+        type: String,
+        choices: {
+        "1": "Survie",
+        "2": "Cauchemar",
+        "3": "Apocalypse"
+        },
+        default: 'e',
+        onChange: value => {
+        // console.log(value);
+        }
+    });
+
+    game.settings.register("engrenages", "first-run-tips-shown", {
+        name: game.i18n.localize("SETTINGS.world.first_run.name"),
+        hint: game.i18n.localize("SETTINGS.world.first_run.hint"),
+        scope: "system",
+        config: true,
+        type: Boolean,
+        default: false
+    });
+}  
